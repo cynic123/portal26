13679b0f6fc95101adb604eeb40bd55202af8b31
prithwish.samanta <prithwish.samanta@gmail.com>
2024-05-04 01:27:27 +0530

    changing to https node package

--- controller/webshrinker/webShrinker.js
+++ controller/webshrinker/webShrinker.js
@@ -1,26 +1,47 @@
+const https = require('https');
+
 module.exports = async (domain) => {
-  try {
+  return new Promise((resolve, reject) => {
     const url = `${process.env.WS_URL}/${Buffer.from(domain).toString('base64')}?taxonomy=webshrinker`;
 
-    const response = await fetch(url, {
+    const options = {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Basic ' + Buffer.from(`${process.env.WS_KEY}:${process.env.WS_SECRET}`).toString('base64')
-      },
-    });
-    
-    if (response.ok) {
-      const jsonData = await response.json();
-      if (jsonData && jsonData.data && jsonData.data.length > 0 && jsonData.data[0].categories) {
-        const categoriesArray = jsonData.data.flatMap(item => item.categories.map(category => category.label));
-        return categoriesArray;
-      } else {
-        throw new Error('Categories not found in response');
       }
-    } else {
-      throw new Error(response.statusText);
-    }
-  } catch (error) {
-    throw new Error(error.message);
-  }
-}
+    };
+
+    const req = https.request(url, options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          const jsonData = JSON.parse(data);
+          if (res.statusCode >= 200 && res.statusCode < 300) {
+            if (jsonData && jsonData.data && jsonData.data.length > 0 && jsonData.data[0].categories) {
+              const categoriesArray = jsonData.data.flatMap(item => item.categories.map(category => category.label));
+              resolve(categoriesArray);
+            } else {
+              reject(new Error('Categories not found in response'));
+            }
+          } else {
+            reject(new Error(`Request failed with status code ${res.statusCode}`));
+          }
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+};
